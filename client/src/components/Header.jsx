ccbcda0a2bfb3639981dc7bdaf1a1a97437f3eaa
44Anas44 <anas4hamadi@gmail.com>
2026-07-22 17:59:14 +0100

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -278,7 +278,7 @@ export default function Header() {
             <Link to="/" className="hdr-logo">
               {LOGO && <img src={LOGO} alt={LOGO_ALT} />}
               <span className="hdr-logo-name">
-               BMS <span>IT</span></>
+               {BRAND.length > 1 ? <>{BRAND.slice(0,-1)}<span>{BRAND.slice(-1)}</span></> : BRAND}
               </span>
             </Link>
 
